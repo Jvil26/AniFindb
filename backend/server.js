53dd9becbc1eff385a292077a8fc27e43cf0975c
Justin Sin <justinsinsin25@gmail.com>
2021-11-27 22:42:48 -0800

    Improve client folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const session = require("express-session");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const rateLimit = require("express-rate-limit");
