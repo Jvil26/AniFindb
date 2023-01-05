1b43feef7b82f0920049857fac6fbce2b50a4fe8
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2023-01-04 17:05:15 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT | 8080;
 
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://anifindb.netlify.app/"],
+    origin: ["http://localhost:3001", "https://anifindb.onrender.com"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
